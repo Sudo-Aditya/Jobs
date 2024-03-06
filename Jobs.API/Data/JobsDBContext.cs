c92d6f1f9857b5f958350329e62debf4ed73a1d0
Aditya <adityaun98@gmail.com>
2024-03-06 21:11:25 +0530

    Second Commit

---
+++ Jobs.API/Data/JobsDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Jobs.API.Models.Domain;
+
+namespace Jobs.API.Data
+{
+    public class JobsDBContext: DbContext
+    {
+        public JobsDBContext(DbContextOptions dbContextOptions): base(dbContextOptions)
+        {
+                
+        }
+
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<JobDetailResponse> JobDetailResponses { get; set; }
+        public DbSet<JobListItem> JobListItems { get; set; }
+        public DbSet<JobListRequest> JobListRequests { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        //public DbSet<Jobs> Jobs { get; set; }
+
+    }
+}
--- Jobs.API/Program.cs
+++ Jobs.API/Program.cs
@@ -1,3 +1,7 @@
+using Jobs.API.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<JobsDBContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("JobsConnectionString")))
 
 var app = builder.Build();
 
