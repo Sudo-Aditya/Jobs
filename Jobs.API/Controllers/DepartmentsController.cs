7ac046f345e48a86f0ebb98f6a646b14de8cd359
Aditya <adityaun98@gmail.com>
2024-03-07 11:21:54 +0530

    Dept Cntrller Fix

--- Jobs.API/Controllers/DepartmentsController.cs
+++ Jobs.API/Controllers/DepartmentsController.cs
@@ -1,10 +1,13 @@
 ﻿using Jobs.API.Data;
+using Jobs.API.Models.Domain;
+using Jobs.API.Models.DTO;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Jobs.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class DepartmentsController : ControllerBase
     {
@@ -15,5 +18,50 @@ namespace Jobs.API.Controllers
             this.dBContext = context;
         }
 
+        //POST /api/v1/departments
+        [HttpPost]
+        public async Task<IActionResult> AddNewDepartment(AddDepartment department)
+        {
+            var departmentData = new Department()
+            {
+                Title = department.Title,
+            };
+            await dBContext.Departments.AddAsync(departmentData);
+            await dBContext.SaveChangesAsync();
+
+            return Ok(departmentData);
+        }
+
+        //PUT /api/v1/departments/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateDepartment(int id, AddDepartment addDepartment)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var existingDepartment = await dBContext.Departments.FindAsync(id);
+
+            if (existingDepartment == null)
+            {
+                return NotFound("Department not found.");
+            }
+
+            //Update properties of existingDepartment
+            existingDepartment.Title = addDepartment.Title;
+
+            dBContext.Departments.Update(existingDepartment);
+            await dBContext.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        //GET: /api/v1/departments
+        [HttpGet]
+        public async Task<IActionResult> GetAllDepartments()
+        {
+            return Ok(await dBContext.Departments.ToListAsync());
+        }
     }
 }
--- Jobs.API/Models/DTO/AddDepartment.cs
+++ Jobs.API/Models/DTO/AddDepartment.cs
@@ -2,5 +2,6 @@
 {
     public class AddDepartment
     {
+        public string Title { get; set; }
     }
 }
